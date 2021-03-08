9adc44cee1f0f462da45b9435539d5ce0b0aee7e
Tim Uzlov <timuzlov@gmail.com>
2021-03-09 00:24:10 +0200

    Refactor cli args parsing changed from sys.args to arparse

--- client.py
+++ client.py
@@ -1,17 +1,29 @@
 import socket
 import select
 import sys
+import argparse
 
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-if len(sys.argv) != 3:
-    print('Incorrect params! Use IP address, port number')
-    exit()
 
-ip_address = str(sys.argv[1])
-port = int(sys.argv[2])
-client.connect((ip_address, port))
+args_parser = argparse.ArgumentParser(prog='TCP chat client',
+                                      description='TCP chat client. Default ip: 127.0.0.1, port: 8081 ')
+args_parser.add_argument('-ip',
+                         type=str,
+                         action='store',
+                         help='Server IP address',
+                         default='127.0.0.1')
+args_parser.add_argument('-port',
+                         '-p',
+                         type=int,
+                         action='store',
+                         help='Server port',
+                         default=8081)
+
+args = args_parser.parse_args()
+
+client.connect((args.ip, args.port))
 
 while True:
     sockets_list = [sys.stdin, client]
@@ -19,11 +31,11 @@ while True:
 
     for socks in read_sockets:
         if socks == client:
-            message = socks.recv(1024)
+            message = socks.recv(2048)
             print(message)
         else:
             message = sys.stdin.readline()
-            client.send(message)
+            client.send(message.encode())
             sys.stdout.write('<You>')
             sys.stdout.write(message)
             sys.stdout.flush()

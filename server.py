af0cb47f2cc2ae9bf1668c29adc3a6b810a72fcf
Tim Uzlov <timuzlov@gmail.com>
2021-03-10 23:27:16 +0200

    Refactor change cli args parsing method: sys.args -> arparse

--- server.py
+++ server.py
@@ -3,7 +3,38 @@ import select
 import sys
 import threading
 import logging
+import argparse
 
+
+args_parser = argparse.ArgumentParser(prog='TCP chat server',
+                                      description='TCP chat server. Default ip: 127.0.0.1, port: 8081 ')
+args_parser.add_argument('-ip',
+                         type=str,
+                         action='store',
+                         dest='ip_address',
+                         help='Server IP address',
+                         default='127.0.0.1')
+args_parser.add_argument('-port',
+                         '-p',
+                         type=int,
+                         action='store',
+                         dest='port',
+                         help='Server port',
+                         default=8081)
+args_parser.add_argument('-c',
+                         type=int,
+                         action='store',
+                         dest='connections',
+                         help='Connections',
+                         default='5')
+args_parser.add_argument('-n',
+                         type=str,
+                         action='store',
+                         dest='chat_room_name',
+                         help='Room name',
+                         default='Room #1')
+
+args = args_parser.parse_args()
 
 logging.basicConfig(level=logging.DEBUG,
                     filename='server.log',
@@ -13,24 +44,14 @@ logging.basicConfig(level=logging.DEBUG,
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-if len(sys.argv) != 5:
-    print('Incorrect params! Use IP address, port number, active connections, chat room name')
-    exit()
-
-ip_address = str(sys.argv[1])
-port = int(sys.argv[2])
-connections = int(sys.argv[3])
-chat_room_name = str(sys.argv[4])
-
-server.bind((ip_address, port))
-server.listen(connections)
-logging.info(f'Started new server(room) {chat_room_name} on {ip_address}:{port}')
+server.bind((args.ip_address, args.port))
+server.listen(args.connections)
+logging.info(f'Started new server(room) {args.chat_room_name} on {args.ip_address}:{args.port}')
 
 active_clients = []
 
 def clientthread(conn, addr):
-    conn.send(f'Welcome to {chat_room_name} chat room')
-
+    conn.send(f'Welcome to {args.chat_room_name} chat room'.encode())
     while True:
         try:
             message = conn.recv(2048)
@@ -47,7 +68,7 @@ def clientthread(conn, addr):
 
 def broadcast(message, connection):
     for clients in active_clients:
-        if clients != connection:
+        if clients!=connection:
             try:
                 clients.send(message)
             except:
@@ -58,17 +79,18 @@ def remove_connection(connection):
     if connection in active_clients:
         active_clients.remove(connection)
         logging.info(f'{connection} removed')
-try:
-    while True:
-        conn, addr = server.accept()
 
-        active_clients.append(conn)
-        print(addr[0] + ' connected')
-        logging.info(f'{addr[0]} connected')
+while True:
+    conn, addr = server.accept()
 
-        threading
-        threading.start_new_thread(clientthread, (conn, addr))
-finally:
-    conn.close()
-    server.close()
-    logging.info(f'Closed new server(room) {chat_room_name} on {ip_address}:{port}')
+    active_clients.append(conn)
+    print(addr[0] + ' connected')
+    logging.info(f'{addr[0]} connected')
+
+    threading.Thread(target=clientthread,
+                     args=(conn, addr)
+                     ).start()
+
+conn.close()
+server.close()
+logging.info(f'Closed new server(room) {chat_room_name} on {ip_address}:{port}')
